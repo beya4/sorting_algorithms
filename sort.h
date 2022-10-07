d0afa7f58eddfb182aaf6fba49d7c55e785e466a
behailu <behailuis4simachew@gmail.com>
2022-10-07 12:08:33 -0700

    quick sort

--- sort.h
+++ sort.h
@@ -44,5 +44,7 @@ void count_sort_LSD(int *array, size_t size, size_t lsd);
 void bitonic_sort(int *array, size_t size);
 void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
 void bitonic_merge(int *array, int l, int r, int direction);
+void quick_sort_hoare(int *array, size_t size);
+
 
 #endif
